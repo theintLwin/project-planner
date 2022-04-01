96669a296202d32e962a2af5ec2068bb70b5af0e
theintLwin <theinttheintlwin38@gmail.com>
2022-04-01 19:29:06 +0630

    adding css and icon to project

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  
   <router-view/>
 </template>
 
@@ -11,7 +8,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  max-width: 600px;
+  margin: 0 auto;
   color: #2c3e50;
 }
 
---
+++ src/views/SingleProject.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="project" >
+    <div class="flexing">
+       
+        <div>
+            <h3 @click="showDetail=!showDetail">{{project.title}}</h3>
+        </div>
+        
+        <div>
+            <i class="fa-solid fa-trash"></i>
+            <i class="fa-solid fa-pen"></i>
+            <i class="fa-solid fa-check"></i>
+        </div>
+    </div>
+    <p v-if="showDetail">{{project.detail}}</p>
+    </div>
+ 
+     
+ 
+  
+</template>
+
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            showDetail:false
+        }
+    },
+    methods:{
+   
+}
+}
+</script>
+
+<style>
+ .project{
+     background-color:#f2f2f2;
+     padding: 20px;
+     margin: 10px;
+     border-left:6px solid crimson;
+     border-radius: 3px;
+ }
+ h3{
+     color: indigo;
+    cursor: pointer;
+
+ }
+ .flexing{
+     display:flex;
+     justify-content: space-between;
+     align-items: center;
+ }
+ i{
+     margin-left:15px;
+    cursor: pointer;
+
+ }
+ i:hover{
+     color:#777;
+ }
+</style>
