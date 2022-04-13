32f8a3876ba1bcfd706895c5b6fb04806c7afc58
theintLwin <theinttheintlwin38@gmail.com>
2022-04-13 10:43:24 +0630

    filter nav with computed property

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
   
     <h1>Home</h1>
     <FilterNav @filterValue="current = $event" :current="current"></FilterNav>
-    <div v-for="project in projects" :key="project.id">
+    <div v-for="project in filteredProjects" :key="project.id">
      <SingleProject :project="project" @delete="deleteProject" @complete="completeProject"></SingleProject>
     </div>
   </div>
@@ -41,6 +41,25 @@ export default {
       result.complete = !result.complete;
     }//this.current = value from (while firing event from child)
   },
+  computed:{
+    filteredProjects(){
+      if(this.current === "complete"){
+        return this.projects.filter((p)=>{
+          return p.complete;
+        })
+        }
+      else if(this.current === "ongoing"){
+        return this.projects.filter((p)=>{
+          return p.complete === false;//!p.complete
+        })
+      }
+      
+       else return this.projects;
+      
+      
+    }
+  },
+
   mounted(){
     fetch('http://localhost:3000/projects')
     .then((response)=>{
